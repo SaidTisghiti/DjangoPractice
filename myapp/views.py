5d1bc65b70ae83e92e4f35f7bf552ba62db9bb26
SAÏD\saids <stisghitie@insjoaquimmir.cat>
2025-12-16 17:35:20 +0100

    Viewing how to use Admin Django

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,25 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+
+def index(request):
+    return HttpResponse("Index page")
+
+def hello(request, username):
+    print(f"Username from URL: {username}")
+    return HttpResponse("<h1>Hello, %s!</h1>" % username)
 
 def about(request):
     return HttpResponse("<h1>About Page</h1><p>This is the about page of our website.</p>")
+
+def projects(request):
+    projects = Project.objects.all()
+    return JsonResponse({'projects': list(projects.values())})
+
+def tasks(request, id):
+    # task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse("task: %s - %s" % (task.title, task.description))
