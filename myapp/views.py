c2a587707b67986548ea311fba448980139dc3b9
SAÏD\saids <stisghitie@insjoaquimmir.cat>
2025-12-16 18:06:54 +0100

    Creating templates to create and use documents html in the project

--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,29 @@
 #from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index page")
+    #return HttpResponse("Index page")
+    return render(request, 'index.html')
 
 def hello(request, username):
     print(f"Username from URL: {username}")
     return HttpResponse("<h1>Hello, %s!</h1>" % username)
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1><p>This is the about page of our website.</p>")
+    #return HttpResponse("<h1>About Page</h1><p>This is the about page of our website.</p>")
+    return render(request, 'about.html')
 
 def projects(request):
     projects = Project.objects.all()
-    return JsonResponse({'projects': list(projects.values())})
+    # return JsonResponse({'projects': list(projects.values())})
+    return render(request, 'projects.html', {'projects': projects})
 
 def tasks(request, id):
     # task = Task.objects.get(id=id)
     task = get_object_or_404(Task, id=id)
-    return HttpResponse("task: %s - %s" % (task.title, task.description))
+    # return HttpResponse("task: %s - %s" % (task.title, task.description))
+    return render(request, 'tasks.html', {'task': task})
